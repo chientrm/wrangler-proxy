90482b861c58ed49d74b094183d23fa28ef754d6
chientrm <chientrm@gmail.com>
2023-11-30 16:55:00 +0000

    D1 Query Bugs

--- src/proxies/d1_database/prepared_statement/raw/proxy.ts
+++ src/proxies/d1_database/prepared_statement/raw/proxy.ts
@@ -3,6 +3,7 @@ import { Proxy } from '../../../proxy';
 
 interface Metadata {
   query: string;
+  values?: any[];
 }
 
 class D1DatabasePreparedStatementRawProxy extends Proxy<Metadata> {
@@ -22,8 +23,10 @@ class D1DatabasePreparedStatementRawProxy extends Proxy<Metadata> {
   async execute(env: any) {
     const { name, metadata } = this,
       d1 = env[name] as D1Database,
-      { query } = metadata,
-      result = await d1.prepare(query).raw();
+      { query, values } = metadata,
+      statement1 = d1.prepare(query),
+      statement2 = values ? statement1.bind(...values) : statement1,
+      result = await statement2.raw();
     return new Response(JSON.stringify(result), jsonInit);
   }
   async receive(response: Response): Promise<any> {
